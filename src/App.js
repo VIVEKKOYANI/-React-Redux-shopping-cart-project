b2a08679fd267c78f73542c5d05a173c11aae78e
vivek koyani <41565769+VIVEKKOYANI@users.noreply.github.com>
2021-04-12 11:39:51 +0530

    add to cart done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Products from './components/Products';
+import { useState } from 'react';
 
 const products = [
   {
@@ -41,10 +42,34 @@ const products = [
 ];
 
 function App() {
+  const [cart, setCart] = useState([{
+    id: 4,
+    name: "Banana",
+    description: "I need those cards",
+    img: "https://tachyons.io/img/avatar_1.jpg",
+    price: 100,
+    units: 2
+  }]);
+
+  const handleAddFunc = (product) => {
+    const existingProduct = cart.filter(p => p.id === product.id);
+
+    if(existingProduct.length > 0){
+      const withoutExistingProduct = cart.filter(p => p.id !== product.id);
+
+      const updatedUnitsProduct = {...existingProduct[0], units: existingProduct[0].units + product.units}
+      setCart([...withoutExistingProduct, updatedUnitsProduct])
+    }else{
+      setCart([...cart, product])
+    }
+  }
   return (
     <main className="pa3 pa5-ns flex flex-wrap">
+      <ul>
+        {cart.map(c => <li>{c.name} | units{c.units}</li>)}
+      </ul>
       {
-        products.map(p => <Products key={p.id} {...p}/>)
+        products.map(p => <Products key={p.id} {...p} addFunc={handleAddFunc}/>)
       }
     </main>
   );
