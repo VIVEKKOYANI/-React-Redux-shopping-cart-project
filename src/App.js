9024511cb1ff73b2eb87efc73837721508b8b110
vivek koyani <41565769+VIVEKKOYANI@users.noreply.github.com>
2021-04-12 12:15:30 +0530

    add data components

--- src/App.js
+++ src/App.js
@@ -2,44 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Products from './components/Products';
 import { useState } from 'react';
-
-const products = [
-  {
-    id: 1,
-    name: "Apple",
-    description: "CCO (Chief Cat Officer)",
-    img: "https://tachyons.io/img/avatar_1.jpg",
-    price: 100
-  },
-  {
-    id: 2,
-    name: "A Cat",
-    description: "CCO (Chief Cat Officer)",
-    img: "https://tachyons.io/img/avatar_1.jpg",
-    price: 100
-  },
-  {
-    id: 3,
-    name: "Milk",
-    description: "Milk for the lactose tollerent",
-    img: "https://tachyons.io/img/avatar_1.jpg",
-    price: 100
-  },
-  {
-    id: 4,
-    name: "Banana",
-    description: "I need those cards",
-    img: "https://tachyons.io/img/avatar_1.jpg",
-    price: 100
-  },
-  {
-    id: 5,
-    name: "Orange",
-    description: "Orange are orange",
-    img: "https://tachyons.io/img/avatar_1.jpg",
-    price: 100
-  }
-];
+import products from './Data';
 
 function App() {
   const [cart, setCart] = useState([{
