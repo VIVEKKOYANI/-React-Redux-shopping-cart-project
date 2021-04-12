0377a76d233b7724c1d89af4af75b049d56ea5ab
vivek koyani <41565769+VIVEKKOYANI@users.noreply.github.com>
2021-04-12 10:41:44 +0530

    add products

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import Products from './components/Products';
+
+const products = [
+  {
+    id: 1,
+    name: "Apple",
+    description: "CCO (Chief Cat Officer)",
+    img: "https://tachyons.io/img/avatar_1.jpg",
+    price: 100
+  },
+  {
+    id: 2,
+    name: "A Cat",
+    description: "CCO (Chief Cat Officer)",
+    img: "https://tachyons.io/img/avatar_1.jpg",
+    price: 100
+  },
+  {
+    id: 3,
+    name: "Milk",
+    description: "Milk for the lactose tollerent",
+    img: "https://tachyons.io/img/avatar_1.jpg",
+    price: 100
+  },
+  {
+    id: 4,
+    name: "Banana",
+    description: "I need those cards",
+    img: "https://tachyons.io/img/avatar_1.jpg",
+    price: 100
+  },
+  {
+    id: 5,
+    name: "Orange",
+    description: "Orange are orange",
+    img: "https://tachyons.io/img/avatar_1.jpg",
+    price: 100
+  }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="pa3 pa5-ns flex flex-wrap">
+      {
+        products.map(p => <Products key={p.id} {...p}/>)
+      }
+    </main>
   );
 }
 
